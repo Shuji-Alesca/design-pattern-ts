f2f9ab737abcb743a2e8744ed7ac0d86c439f2ac
Shuji Kunishige <shuji.kunishige@alescalife.com>
2023-10-14 04:38:47 +0000

    simplify overload method

--- src/13Visitor/Visitor.ts
+++ src/13Visitor/Visitor.ts
@@ -1,8 +1,6 @@
 import { type Directory } from './Directory'
 import { type File } from './File'
 
-export abstract class Visitor {
-  public abstract visit (target: File | Directory): void
-  public abstract visit (file: File): void
-  public abstract visit (directory: Directory): void
+export interface Visitor {
+  visit: (...args: [target: File | Directory]) => void
 }
